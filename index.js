58fbb04d26305ef2c65084fcea3f1c3866af8b46
root <root@localhost.localdomain>
2021-06-22 23:23:53 +0100

    feat: add partials files on views

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bodyParser from "body-parser";
 import cors from "cors";
 
 import db from "./config/db.js";
@@ -10,6 +11,22 @@ app.use(cors());
 
 // - register view engine -
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("projects/index", { title: "Home" });
+});
+
+// app.get("/", (req, res) => {
+//   res.redirect("/blogs");
+// });
+
+//app.use("/blogs", blogRoutes);
+
+// - 404 Page -
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
+});
 
 const PORT = process.env.PORT || 5000;
 
