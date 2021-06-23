9f81f6961d08ba852c69757bc87fbc10a628cbd9
root <root@localhost.localdomain>
2021-06-23 15:38:39 +0100

    refactor ! : config the .env file

--- config/db.js
+++ config/db.js
@@ -1,10 +1,13 @@
 import mysql from "mysql";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "cms-portofolio",
+  host: process.env.MYSQL_ADDON_HOST,
+  user: process.env.MYSQL_ADDON_USER,
+  password: process.env.MYSQL_ADDON_PASSWORD,
+  database: process.env.MYSQL_ADDON_DB,
 });
 
 export default db;
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import db from "./config/db.js";
 import projectsRoutes from "./routes/projectsRoutes.js";
 
 const app = express();
+
+dotenv.config();
+
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
